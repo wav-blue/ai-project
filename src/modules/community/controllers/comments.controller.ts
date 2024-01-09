d0946671c5aa26213361834bbd0712be005092f1
HY 'Yenny' Kim <o2hykim@gmail.com>
2024-01-10 02:07:49 +0900

    fix: comments controller endpoint 경미한 수정

--- src/modules/community/controllers/comments.controller.ts
+++ src/modules/community/controllers/comments.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { CommentsService } from '../services/comments.service';
 
-@Controller('posts')
+@Controller('comments')
 export class CommentsController {
   constructor(private commentsService: CommentsService) {}
 }
