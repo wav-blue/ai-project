8b7ec3a2707c75e2d68bdd7af1df1f79cda926a3
박수진 <wav0120@gmail.com>
2024-01-11 01:53:03 +0900

    synchronize: false로 변경, console.log 삭제

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -5,15 +5,6 @@ import * as config from 'config';
 dotenv.config();
 const dbConfig = config.get('db');
 
-console.log(
-  'db 정보',
-  process.env.RDS_HOSTNAME,
-  process.env.RDS_PORT || dbConfig.port,
-  process.env.RDS_USERNAME,
-  process.env.RDS_PASSWORD,
-  process.env.RDS_DB_NAME,
-);
-
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
   host: process.env.RDS_HOSTNAME,
