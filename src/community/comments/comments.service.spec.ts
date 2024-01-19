c46147aca9a0c9fce54a21b81e1682834fd12c9f
박수진 <wav0120@gmail.com>
2024-01-19 23:57:56 +0900

    Test: comment Service 테스트 작성 중

--- src/community/comments/comments.service.spec.ts
+++ src/community/comments/comments.service.spec.ts
@@ -1,27 +1,46 @@
-import { TestBed } from '@automock/jest';
 import { CommentsService } from './comments.service';
-import { CommentRepository } from './comments.repository';
 import { DataSource } from 'typeorm';
 import { CreateCommentDto } from './dto/create-comment.dto';
+
+jest.mock('./comments.repository');
+import { CommentRepository } from './comments.repository';
+import { Logger } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
 
 describe('commentService Transaction Test', () => {
   let commentsService: CommentsService;
   let commentRepository: CommentRepository;
   let dataSource: DataSource;
+  let httpService: HttpService;
 
   // beforeAll? berforeEach?
   beforeAll(() => {
-    commentsService = new CommentsService(commentRepository, dataSource);
+    commentsService = new CommentsService(
+      commentRepository,
+      dataSource,
+      httpService,
+    );
   });
 
   // comment 생성 테스트
+  // async createComment(user: string, createCommentDto: CreateCommentDto) {
   describe('transaction test', () => {
-    it('should return an array of comments(for testing)', async () => {
-      const createCommentDto = new CreateCommentDto();
-      createCommentDto.boardId = 5; // 유효하지 않은 게시판 번호
-      createCommentDto.content = 'test Content';
+    it('request success라는 응답을 전송해야 함', async () => {
+      const testUser = '7238bf28-4db1-4bc2-a941-023badad9bdf';
+      const testCreateCommentDto = new CreateCommentDto();
+      testCreateCommentDto.boardId = 5;
+      testCreateCommentDto.content = 'test Content';
 
-      commentsService.createComment('testUserA', createCommentDto);
+      new Logger.log(
+        'reqest 데이터\n testUser: ',
+        testUser,
+        'Dto: ',
+        testCreateCommentDto,
+      );
+
+      commentsService.createComment(testUser, testCreateCommentDto);
+
+      expect('request success');
     });
   });
 });
