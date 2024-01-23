b0aed8305df0e2f0bfc256a5cab10a6fd509cde7
박수진 <wav0120@gmail.com>
2024-01-23 14:13:49 +0900

    Test: 테스트 코드 수정

--- src/community/comments/comments.controller.spec.ts
+++ src/community/comments/comments.controller.spec.ts
@@ -2,39 +2,47 @@ import { DataSource } from 'typeorm';
 import { CommentsController } from './comments.controller';
 import { CommentRepository } from './comments.repository';
 import { CommentsService } from './comments.service';
+import { CreateCommentReportDto } from './dto/create-comment-report.dto';
+import { HttpService } from '@nestjs/axios';
 
+// 작동 안함
 describe('commentsController', () => {
   let commentsController: CommentsController;
   let commentsService: CommentsService;
   let commentRepository: CommentRepository;
   let dataSource: DataSource;
+  let httpService: HttpService;
 
   beforeEach(() => {
-    commentsService = new CommentsService(commentRepository, dataSource);
+    commentsService = new CommentsService(
+      commentRepository,
+      dataSource,
+      httpService,
+    );
     commentsController = new CommentsController(commentsService);
   });
 
-  // 모든 댓글 조회 테스트
-  describe('findAllComments', () => {
-    it('should return an array of comments(for testing)', async () => {
-      const result = [];
-      jest
-        .spyOn(commentsService, 'getAllComments')
-        .mockImplementation(async () => result);
-
-      expect(await commentsController.getAllComments()).toBe(result);
-    });
-  });
-
+  // 신고내역 생성 테스트
   // 포스트의 댓글 조회 테스트
-  describe('findBoardComments', () => {
+  describe('신고 내역을 성공적으로 생성!', () => {
     it('should return an array of comments of board', async () => {
-      const result = [];
-      jest
-        .spyOn(commentsService, 'getBoardComments')
-        .mockImplementation(async () => result);
+      const result = 'request success';
+      const testUserId = '7bc1d0d8-3127-4781-9154-35fef0402e51';
+      const testBody = {
+        commentId: 27,
+        reportType: '의심스러운 내용입니다.',
+      };
+      let testCreateCommentReportDto = new CreateCommentReportDto();
+      testCreateCommentReportDto = testBody;
 
-      expect(await commentsController.getBoardComments(3)).toBe(result);
+      console.log('DTO 확인', testCreateCommentReportDto);
+
+      expect(
+        await commentsController.createCommentReport(
+          testUserId,
+          testCreateCommentReportDto,
+        ),
+      ).toBe(result);
     });
   });
 });
