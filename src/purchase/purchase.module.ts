dd4f09587ea478e6e5d280bd17e4619650b72313
[AI9] 최원민 <wonmin98217@gmail.com>
2024-01-25 11:06:01 +0900

    refactor: user repo

--- src/purchase/purchase.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PassportModule } from '@nestjs/passport';
-
-import * as config from 'config';
--- src/purchase/purchase.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-import { DataSource, QueryRunner } from 'typeorm';
-
-@Injectable()
-export class PurchaseService {
-  constructor(private dataSource: DataSource) {}
-}
