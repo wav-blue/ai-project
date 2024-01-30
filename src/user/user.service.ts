abab7baa59e26360d54111c1e1535968a1cfbdf6
HY 'Yenny' Kim <o2hykim@gmail.com>
2024-01-30 23:01:37 +0900

    fix: merge 충돌 얼렁뚱땅 해결

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,7 +6,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 
-import { DataSource } from 'typeorm';
+import { DataSource, QueryRunner } from 'typeorm';
 import { UserRepository } from './user.repository';
 import { RefreshTokenRepository } from './refreshtoken.repository';
 import { User } from './user.entity';
@@ -26,114 +26,14 @@ dotenv.config();
 export class UserService {
   constructor(
     private dataSource: DataSource,
-    // @InjectRepository(User)
     private userRepository: UserRepository,
 
-    // @InjectRepository(RefreshToken)
     private refreshTokenRepository: RefreshTokenRepository,
     private jwt: JwtService,
   ) {}
 
-  async connectDB(operation: () => Promise<any>): Promise<any> {
-    const queryRunner = await this.dataSource.createQueryRunner();
-    try {
-      await queryRunner.connect();
-      await queryRunner.startTransaction();
-      await operation(); // 비동기 작업 앞에 await 추가
-      await queryRunner.commitTransaction();
-    } catch (error) {
-      await queryRunner.rollbackTransaction();
-      throw new InternalServerErrorException('데이터베이스 처리 중 오류 발생');
-    } finally {
-      await queryRunner.release();
-    }
-  }
-
-  // async getUserById(userId: string): Promise<User> {
-  //   const found = await this.userRepository.getUserbyId(userId);
-  //   if (!found) throw new UnauthorizedException('존재하지 않는 계정입니다.');
-  //   return found;
-  // }
-
-  // async createUser(createUserDto: CreateUserDto): Promise<User> {
-  //   const found = await this.userRepository.getUserbyEmail(createUserDto.email);
-  //   if (found) throw new ConflictException('이미 존재하는 이메일입니다.');
-
-  //   createUserDto.password = await bcrypt.hash(createUserDto.password, 10);
-
-  //   const createdUser = await this.userRepository.createUser(createUserDto);
-  //   return createdUser;
-  // }
-
-  // async updateUser(updateUserDto: UpdateUserDto): Promise<User> {
-  //   const found = await this.userRepository.getUserbyId(updateUserDto.userId);
-  //   if (!found) throw new BadRequestException('존재하지 않는 계정입니다.');
-
-  //   updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
-
-  //   const updateUser = await this.userRepository.updateUser(updateUserDto);
-  //   return updateUser;
-  // }
-
-  // async userLogin(
-  //   loginUser: LoginUserDto,
-  // ): Promise<{ user: User; accessToken: string; refreshToken: string }> {
-  //   let found = await this.userRepository.getUserbyEmail(loginUser.email);
-
-  //   // 없는 유저면 DB에 유저정보 저장
-  //   if (!found) {
-  //     if (loginUser.logintype != 'EMAIL') {
-  //       found = await this.userRepository.createUser(loginUser);
-  //     } else {
-  //       throw new BadRequestException('존재하지 않는 계정입니다.');
-  //     }
-  //   }
-
-  //   const { accessToken, refreshToken } = await this.TokenCreate(found.user_id);
-
-  //   return { user: found, accessToken, refreshToken };
-  // }
-
-  // async findUserRefreshToken(userId: string): Promise<RefreshToken> {
-  //   const userRefreshToken =
-  //     await this.refreshTokenRepository.getRefreshTokenbyUserId(userId);
-
-  //   return userRefreshToken;
-  // }
-
-  // async TokenCreate(
-  //   user_id: string,
-  // ): Promise<{ accessToken: string; refreshToken: string }> {
-  //   const findUserPayload = { user_id: user_id };
-
-  //   const accessToken = this.jwt.sign(findUserPayload, {
-  //     expiresIn: 600,
-  //   });
-
-  //   const refreshToken = this.jwt.sign(findUserPayload, {
-  //     expiresIn: 3600,
-  //   });
-
-  //   const foundRefeshToken =
-  //     await this.refreshTokenRepository.getRefreshTokenbyUserId(user_id);
-
-  //   if (foundRefeshToken) {
-  //     await this.refreshTokenRepository.updateRefreshToken(
-  //       user_id,
-  //       refreshToken,
-  //     );
-  //   } else {
-  //     await this.refreshTokenRepository.createRefreshToken(
-  //       user_id,
-  //       refreshToken,
-  //     );
-  //   }
-
-  //   return { accessToken, refreshToken };
-  // }
-
   async getUserById(userId: string): Promise<User> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const queryRunner = await this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
     try {
@@ -143,14 +43,14 @@ export class UserService {
       return found;
     } catch (error) {
       await queryRunner.rollbackTransaction();
-      throw error;
+      throw new InternalServerErrorException('데이터베이스 처리 중 오류 발생');
     } finally {
       await queryRunner.release();
     }
   }
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const queryRunner = await this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
     try {
@@ -170,14 +70,14 @@ export class UserService {
       return createdUser;
     } catch (error) {
       await queryRunner.rollbackTransaction();
-      throw error;
+      throw new InternalServerErrorException('데이터베이스 처리 중 오류 발생');
     } finally {
       await queryRunner.release();
     }
   }
 
   async updateUser(updateUserDto: UpdateUserDto): Promise<User> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const queryRunner = await this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
     try {
@@ -194,7 +94,47 @@ export class UserService {
         queryRunner,
       );
       await queryRunner.commitTransaction();
+
       return updateUser;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException('데이터베이스 처리 중 오류 발생');
+    } finally {
+      await queryRunner.release();
+    }
+  }
+  async userLoginEmail(
+    loginUser: LoginUserDto,
+  ): Promise<{ user: User; accessToken: string; refreshToken: string }> {
+    const queryRunner = await this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const found = await this.userRepository.getUserbyEmail(
+        loginUser.email,
+        queryRunner,
+      );
+      // 없는 유저면 DB에 유저정보 저장
+      if (!found) {
+        throw new BadRequestException('존재하지 않는 계정입니다.');
+      }
+
+      const passwordcorrect = await bcrypt.compare(
+        loginUser.password,
+        found.password,
+      );
+
+      if (!passwordcorrect) {
+        throw new BadRequestException('잘못된 비밀번호입니다.');
+      }
+
+      const { accessToken, refreshToken } = await this.TokenCreate(
+        found.userId,
+        queryRunner,
+      );
+      await queryRunner.commitTransaction();
+
+      return { user: found, accessToken, refreshToken };
     } catch (error) {
       await queryRunner.rollbackTransaction();
       throw error;
@@ -203,10 +143,10 @@ export class UserService {
     }
   }
 
-  async userLogin(
+  async userLoginSocial(
     loginUser: LoginUserDto,
   ): Promise<{ user: User; accessToken: string; refreshToken: string }> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const queryRunner = await this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
     try {
@@ -214,18 +154,14 @@ export class UserService {
         loginUser.email,
         queryRunner,
       );
-
       // 없는 유저면 DB에 유저정보 저장
       if (!found) {
-        if (loginUser.logintype != 'EMAIL') {
-          found = await this.userRepository.createUser(loginUser, queryRunner);
-        } else {
-          throw new BadRequestException('존재하지 않는 계정입니다.');
-        }
+        found = await this.userRepository.createUser(loginUser, queryRunner);
       }
 
       const { accessToken, refreshToken } = await this.TokenCreate(
         found.userId,
+        queryRunner,
       );
       await queryRunner.commitTransaction();
 
@@ -239,7 +175,7 @@ export class UserService {
   }
 
   async findUserRefreshToken(userId: string): Promise<RefreshToken> {
-    const queryRunner = this.dataSource.createQueryRunner();
+    const queryRunner = await this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
     try {
@@ -249,7 +185,8 @@ export class UserService {
           queryRunner,
         );
       await queryRunner.commitTransaction();
-      return userRefreshToken;
+
+      return userRefreshToken; // 비동기 작업 앞에 await 추가
     } catch (error) {
       await queryRunner.rollbackTransaction();
       throw error;
@@ -260,10 +197,8 @@ export class UserService {
 
   async TokenCreate(
     userId: string,
+    queryRunner: QueryRunner,
   ): Promise<{ accessToken: string; refreshToken: string }> {
-    const queryRunner = this.dataSource.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
     try {
       const findUserPayload = { userId: userId };
 
@@ -294,13 +229,9 @@ export class UserService {
           queryRunner,
         );
       }
-      await queryRunner.commitTransaction();
       return { accessToken, refreshToken };
     } catch (error) {
-      await queryRunner.rollbackTransaction();
       throw error;
-    } finally {
-      await queryRunner.release();
     }
   }
 }
