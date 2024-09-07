a151885b8613814d4b6a535b3b445eb69e143d72
박수진 <wav0120@gmail.com>
2024-09-07 15:35:58 +0900

    Refactor: flask 서버 사용하지 않는 코드 삭제

--- ai/app.py
+++ ai/app.py
@@ -1,13 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from services.gpt_service import conversation
-import re, random
-from utils.gpt_util import generate_random_id, parsing_gpt_answer
 from utils.model_util import parse_response_by_label, validate_by_base64
-import tensorflow as tf
-from flask import g
-from transformers import TFBertModel, pipeline , BertTokenizer, TFBertForSequenceClassification
-import base64
+from transformers import pipeline , BertTokenizer
 
 app = Flask(__name__)
 CORS(app)
@@ -30,60 +24,6 @@ classifier = load_model()
 def index():
   return render_template('index.html')
 
-# 첫 채팅 API
-@app.route('/chat/first', methods=['POST'])
-def firstConversation():
-
-  history = []
-  requestData = request.json
- 
-  # GPT에게 질문 전달, 응답 저장
-  response_message = conversation(requestData)
-
-  # title 저장
-  title = response_message.split('\n')[0]
-
-  # 임시로 생성한 10자리 난수 id
-  chatId = generate_random_id()
-
-  # [chatId, title] history [0]에 저장
-  chatInfo = [chatId, title]
-  history.append(chatInfo)
-
-  # 답변에서 '[상담]:' 다음 부분만 파싱
-  counselingText = parsing_gpt_answer(response_message)
-
-  # history에 첫 문답 저장
-  chat_history = [requestData['question'], counselingText]
-  history.append([])
-  history[1].append(chat_history)
-  print('history:', history)
-
-  # message 객체의 content 속성을 사용
-  return jsonify({"response": history})
-
-
-# 추가채팅 API
-@app.route('/chat/<chatId>', methods=['POST'])
-def additionalConversation(chatId):
-
-  history = request.json['history']
-  requestData = request.json
- 
-  # GPT에게 질문 전달, 응답 저장
-  response_message = conversation(requestData)
-
-  # 답변에서 '[상담]:' 다음 부분만 파싱
-  counselingText = parsing_gpt_answer(response_message)
-
-  # history에 새 문답 저장
-  chat_history = [requestData['question'], counselingText]
-  history[1].append(chat_history)
-  print('history:', history)
-
-  # message 객체의 content 속성을 사용
-  return jsonify({"response": history})
-
 # 모델을 이용하여 긍부정 분류
 @app.route('/analysis', methods=['POST'])
 def textAnalysis():
@@ -97,7 +37,6 @@ def textAnalysis():
     password = auth.split(' ')[1].split(':')[1]
 
     if not validate_by_base64(username, password):
-      print("불일치!!")
       return 'Unauthroization!', 401
     else:
       print("인증 성공")
