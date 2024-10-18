0c1bc8c03d09fb64a9dde8844b7ca7551b602cac
박수진 <wav0120@gmail.com>
2024-10-18 13:41:43 +0900

    Fix: Logger Service 설정 사항 변경

--- back/src/logger/wintonLogger.ts
+++ back/src/logger/wintonLogger.ts
@@ -1,14 +1,13 @@
-// const winston = require('winston');
 import winston = require('winston');
 import winstonDaily = require('winston-daily-rotate-file');
 import process = require('process');
 
 const { combine, timestamp, label, printf } = winston.format;
 
-//* 로그 파일 저장 경로 → 루트 경로/logs 폴더
+// 로그 파일 저장 경로 → 루트 경로/logs 폴더
 const logDir = `${process.cwd()}/logs`;
 
-//* log 출력 포맷 정의 함수
+// log 출력 포맷 정의 함수
 const logFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp} [${label}] ${level}: ${message}`;
 });
@@ -18,23 +17,23 @@ const logFormat = printf(({ level, message, label, timestamp }) => {
  * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
  */
 const logger = winston.createLogger({
-  //* 로그 출력 형식 정의
+  // 로그 출력 형식 정의
   format: combine(
     timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     label({ label: 'GuruBack' }), // 어플리케이션 이름
     logFormat,
   ),
-  //* 실제 로그를 어떻게 기록을 한 것인가 정의
+  // 실제 로그를 어떻게 기록을 한 것인가 정의
   transports: [
     new winstonDaily({
-      level: 'warn', // info 이상의 레벨 저장
+      level: 'info', // info 이상의 레벨 저장
       datePattern: 'YYYY-MM-DD', // 파일 날짜 형식
       dirname: logDir, // 파일 경로
       filename: `%DATE%.log`, // 파일 이름
       maxFiles: 30, // 최근 30일치 로그 파일을 남김
       zippedArchive: true,
     }),
-    //* error 레벨 로그를 저장할 파일 설정
+    // error 레벨 로그를 저장할 파일 설정
     new winstonDaily({
       level: 'error',
       datePattern: 'YYYY-MM-DD',
@@ -44,7 +43,7 @@ const logger = winston.createLogger({
       zippedArchive: true,
     }),
   ],
-  //* uncaughtException 발생시 파일 설정
+  // uncaughtException 발생시 파일 설정
   exceptionHandlers: [
     new winstonDaily({
       level: 'error',
